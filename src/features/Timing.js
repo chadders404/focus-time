3b5122c78a6738b7b4be6d27b7cdfcf4c9a62c00
chadders404 <chadwick1996@gmail.com>
2022-07-07 21:43:44 +0100

    Removed unecessary imports

--- src/features/Timing.js
+++ src/features/Timing.js
@@ -5,22 +5,22 @@ import { RoundedButton } from "../Components/RoundedButton";
 export const Timing = ({ onChangeTime }) => {
   return (
     <>
-<View style={styles.timingButton}>
- <RoundedButton size={75} title="10" onPress={()=>onChangeTime(10)}/>
-</View>
-<View style={styles.timingButton}>
- <RoundedButton size={75} title="20" onPress={()=>onChangeTime(20)}/>
-</View>
-<View style={styles.timingButton}>
- <RoundedButton size={75} title="25" onPress={()=>onChangeTime(25)}/>
-</View>
-</>
-  )
-}
+      <View style={styles.timingButton}>
+        <RoundedButton size={75} title="10" onPress={() => onChangeTime(10)} />
+      </View>
+      <View style={styles.timingButton}>
+        <RoundedButton size={75} title="20" onPress={() => onChangeTime(20)} />
+      </View>
+      <View style={styles.timingButton}>
+        <RoundedButton size={75} title="25" onPress={() => onChangeTime(25)} />
+      </View>
+    </>
+  );
+};
 
-const styles=StyleSheet.create ({
+const styles = StyleSheet.create({
   timingButton: {
     flex: 1,
-    alignItems: "center"
-  }
-})
+    alignItems: "center",
+  },
+});
